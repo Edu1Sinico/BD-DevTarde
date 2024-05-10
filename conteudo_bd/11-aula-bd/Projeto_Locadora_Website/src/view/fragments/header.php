8363431b4c6435c3b09358c9cce10f0ec116c268
Edu1Sinico <EduardoSenai888@gmail.com>
2024-05-10 15:22:55 -0300

    Enviando o projeto para o github

---
+++ conteudo_bd/11-aula-bd/Projeto_Locadora_Website/src/view/fragments/header.php
@@ -0,0 +1,33 @@
+<header class="header-fragment">
+    <div class="header-section-logo">
+        <div class="div-logo">
+            <img src="" alt="Logo">
+        </div>
+    </div>
+    <div class="header-section-menu">
+        <div class="div-menu">
+            <div class="div-menu-contact-login-register">
+                <div class="div-contact">
+                    CONTATO: (00) 00000-0000
+                </div>
+                <div class="div-btn-login-register">
+                    <div class="div-login">
+                        <button class="login-btn">Login</button>
+                    </div>
+                    <div class="div-register">
+                        <button class="register-btn">Cadastrar</button>
+                    </div>
+                </div>
+            </div>
+            <nav class="nav-links">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Sobre Nós</a></li>
+                    <li><a href="#">Veículos</a></li>
+                    <li><a href="#">Nossas Lojas</a></li>
+                    <li><a href="#">Fale Conosco</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
---
+++ conteudo_bd/11-aula-bd/Projeto_Locadora_Website/src/view/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Locadora - Home</title>
+    <link rel="stylesheet" href="../templates/fragmentsCSS/styleHeader.css">
+    <link rel="stylesheet" href="../templates/fragmentsCSS/styleFooter.css">
+    <link rel="stylesheet" href="../templates/styleHome.css">
+</head>
+<body>
+    <?php include 'fragments/header.php'; ?>
+</body>
+</html>
