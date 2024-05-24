65598ca017e048fee5ab15d08e846a661470e21a
Edu1Sinico <EduardoSenai888@gmail.com>
2024-05-24 16:24:25 -0300

    Enviando a página home atualizada para o github

--- conteudo_bd/11-aula-bd/Projeto_Locadora_Website/src/view/index.php
+++ conteudo_bd/11-aula-bd/Projeto_Locadora_Website/src/view/index.php
@@ -12,12 +12,33 @@
 </head>
 
 <body>
+    <div class="div-header-import">
+        <?php include 'fragments/header.php'; ?>
+    </div>
     <main>
-        <div class="div-header-import">
-            <?php include 'fragments/header.php'; ?>
-        </div>
         <div class="div-banner-section">
             <img src="../assets/banner/locadora-banner.png" alt="banner">
+        </div>
+        <div class="div-about-us">
+            <div class="div-about-us-1">
+                <div class="div-about-us-title">
+                    <h1 class="about-us-title">BEM-VINDO AO</h1>
+                    <h1 class="about-us-subtitle">NOSSO SITE</h1>
+                </div>
+                <div class="div-about-us-info">
+                    <p class="about-us-info">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+                <div class="div-about-us-button">
+                    <button>Saiba Mais</button>
+                </div>
+            </div>
+            <div class="div-about-us-2">
+                <div class="div-about-us-img">
+                    <img src="" alt="exemple">
+                </div>
+            </div>
         </div>
     </main>
     <div class="div-footer-import">
