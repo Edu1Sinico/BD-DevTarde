182f2c74e74feaf53dfeb426ea6c69cefa29b0cd
Edu1Sinico <EduardoSenai888@gmail.com>
2024-06-19 20:26:25 -0300

    Enviando o projeto para o githbu

--- conteudo_bd/11-aula-bd/Projeto_Locadora_Website/src/fragments/header.php
+++ conteudo_bd/11-aula-bd/Projeto_Locadora_Website/src/fragments/header.php
@@ -19,25 +19,31 @@ if (session_status() == PHP_SESSION_NONE) {
                     <a href="#"><i class="fas fa-regular fa-laptop"></i></a>
                 </div>
                 <div class="div-btn-login-register">
-                    <?php if (isset($_SESSION['user_id'])) : ?>
-                        <div class="div-user-info">
-                            <span>Bem-vindo, <?php echo htmlspecialchars($_SESSION['user_name'], ENT_QUOTES, 'UTF-8'); ?></span>
-                            <a href="controller/logoutController.php"><button class="logout-btn">Sair</button></a>
-                        </div>
-                        <?php if ($_SESSION['user_tipo'] == 'funcionario') : ?>
-                            <div class="div-extra-links">
-                                <a href="../registerCars.php"><button class="extra-btn">Cadastro de Veículos</button></a>
-                                <a href="../registerWorkers.php"><button class="extra-btn">Cadastro de Funcionários</button></a>
-                            </div>
-                        <?php endif; ?>
-                    <?php else : ?>
-                        <div class="div-login">
+                    <?php // if (isset($_SESSION['user_id'])) : 
+                    ?>
+                    <div class="div-user-info">
+                        <span>Bem-vindo, <?php //echo htmlspecialchars($_SESSION['user_name'], ENT_QUOTES, 'UTF-8'); 
+                                            ?></span>
+                        <a href="controller/logoutController.php"><button class="logout-btn"></button></a>
+                    </div>
+                    <?php // if ($_SESSION['user_tipo'] == 'funcionario') : 
+                    ?>
+                    <div class="div-extra-links">
+                        <a href="../registerCars.php"><button class="extra-btn">Cadastro de Veículos</button></a>
+                        <a href="../registerWorkers.php"><button class="extra-btn">Cadastro de Funcionários</button></a>
+                    </div>
+                    <?php // endif; 
+                    ?>
+                    <?php // else : 
+                    ?>
+                    <!-- <div class="div-login">
                             <a href="../loginUser.php"><button class="login-btn">Login</button></a>
                         </div>
                         <div class="div-register">
                             <a href="../registerUser.php"><button class="register-btn">Cadastrar</button></a>
-                        </div>
-                    <?php endif; ?>
+                        </div> -->
+                    <?php //endif; 
+                    ?>
                 </div>
             </div>
             <nav class="nav-links">
---
+++ conteudo_bd/11-aula-bd/Projeto_Locadora_Website/src/listCarsPage.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Autoroad - Veículos</title>
+    <link rel="stylesheet" href="templates/fragmentsCSS/styleHeader.css">
+    <link rel="stylesheet" href="templates/fragmentsCSS/styleFooter.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Seção header -->
+    <div class="div-header-import">
+        <?php include 'fragments/header.php'; ?>
+    </div>
+
+    <!-- Seção principal -->
+    
+
+    <!-- Seção footer -->
+    <div class="div-footer-import">
+        <?php include 'fragments/footer.php'; ?>
+    </div>
+</body>
+
+</html>
